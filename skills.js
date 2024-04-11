cfa9c7031c1346d5c6166f603e1997883d633b67
Anthony Chu <lijiechu@uw.edu>
2024-04-11 12:50:01 -0700

    add function

--- skills.js
+++ skills.js
@@ -5,3 +5,7 @@ function calculateNumbers(var1, var2) {
 function multiplyNumbers(var1, var2) {
     console.log(var1 * var2);
 }
+
+function divideNumbers(var1, var2) {
+    console.log(var1 / var2);
+}
